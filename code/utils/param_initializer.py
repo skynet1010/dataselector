cbff4256f5636967fa15305d2c08bcc91006cb0e
Andreas Klos <andreas.klos@fernuni-hagen.de>
2020-04-23 00:23:40 +0200

    FIX same bug in param_ini.py

--- code/utils/param_initializer.py
+++ code/utils/param_initializer.py
@@ -1,6 +1,6 @@
 import sys
 from utils.postgres_functions import table_row_sql, insert_row
-
+from utils.consts import ds_results_table_name, state_table_name
 
 def init_analysis_params(args, conn, cur, task):
     retrain = False
@@ -8,10 +8,10 @@ def init_analysis_params(args, conn, cur, task):
     niteration = nepoch = 1
     best_acc = 0.0
     best_loss = best_exec_time = sys.float_info.max
-    cur.execute(table_row_sql(args.ds_results_table_name, args.ds_results_table_name, task))
+    cur.execute(table_row_sql(ds_results_table_name, task))
     res = cur.fetchall()
     if res == []:
-        cur.execute(insert_row(args.ds_results_table_name, args.ds_results_table_name, task, niteration, nepoch, best_acc, best_loss, best_exec_time))
+        cur.execute(insert_row(ds_results_table_name, task, niteration, nepoch, best_acc, best_loss, best_exec_time))
         conn.commit()
     else:
         _, _, best_acc, best_loss, best_exec_time = res[0]
@@ -19,7 +19,7 @@ def init_analysis_params(args, conn, cur, task):
         retrain=True
 
     if retrain:
-        cur.execute(table_row_sql(args.state_table_name, args.ds_results_table_name, task))
+        cur.execute(table_row_sql(state_table_name, task))
         res = cur.fetchall()
         if res != []:
             niteration, nepoch, _, _ = res[0]
