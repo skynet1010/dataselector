c746371f7eed46ff57f1ad406fbafb26e221df92
Andreas Klos <andreas.klos@fernuni-hagen.de>
2020-11-21 22:08:14 +0100

    adjust task queue

--- code/task_listener.py
+++ code/task_listener.py
@@ -53,7 +53,7 @@ def start_task_listener(args):
 
     channel = connection.channel()
 
-    channel.queue_declare(queue="task_queue_final",durable=True)
+    channel.queue_declare(queue="task_queue",durable=True)
 
     print(" [*] Waiting for tasks. To exit press CTRL+C")
 
